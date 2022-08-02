f604bafa824703a26586a4de311f8ba6984f9401
Criscamgel <caguio@ucompensar.edu.co>
2022-08-02 11:14:03 -0500

    merge

--- Angular/01-bases/src/app/app.component.ts
+++ Angular/01-bases/src/app/app.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'bases';
+  titulo: string = 'Contador App';
+  numero: number = 10;
 }
