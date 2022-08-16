98016c7ee4c0bbe925573cf46d1066404524aada
Criscamgel <caguio@ucompensar.edu.co>
2022-08-15 21:36:24 -0500

    Generando el dist de produccion

--- Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
+++ Angular/01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -7,23 +7,10 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class MainPageComponent {
 
-  personajes:Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegueta',
-      poder: 7500
-    }
-  ]
-
   nuevo: Personaje = {
     nombre: 'Trunks',
     poder: 14000
   }
 
-  agregarNuevoPersonaje(nuevoPersonaje:Personaje){
-    this.personajes.push(nuevoPersonaje);
-  }
+  constructor() {}
 }
